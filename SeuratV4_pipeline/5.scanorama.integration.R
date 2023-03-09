2dbb50446155d55c47110c2acc65ca9733672bfb
CozyGalaxy <98370403+CozyGalaxy@users.noreply.github.com>
2023-03-09 11:17:32 +0800

    Update 5.scanorama.integration.R

--- SeuratV4_pipeline/5.scanorama.integration.R
+++ SeuratV4_pipeline/5.scanorama.integration.R
@@ -173,7 +173,7 @@ if(!is.null(args$marker)){
 	colnames(sample_marker) = c("marker", "anno")
 	plot_theme<-theme(panel.background = element_blank(),axis.line = element_line(size=0.1),axis.ticks = element_line(size=0.1),axis.text = element_text(size=7),axis.text.x = element_text(angle = 45, vjust = 1, hjust = 1),axis.ticks.length = unit(1, "pt"),plot.title = element_text(size = 7, face = "bold",margin=margin(0,0,4,0)),plot.margin = unit(c(0,0,0,0),"cm"),legend.key.size = unit(0.1, 'cm'),legend.key.height = unit(0.1, 'cm'),legend.key.width = unit(0.1, 'cm'),legend.title = element_text(size=6),legend.text = element_text(size=6))
 
-	dot_p <- DotPlot(data_merge, assay="RNA", features=sample_marker$marker,cols = c("grey","red"),col.min= -0.5,dot.scale=3)+labs(x="",y="")+plot_theme
+	dot_p <- DotPlot(data_merge, features=sample_marker$marker,cols = c("grey","red"),col.min= -0.5,dot.scale=3)+labs(x="",y="")+plot_theme
 	ggsave(filename = paste0(args$out,"/", args$sample, ".Marker.DotPlot.pdf"), plot = dot_p, device = "pdf", width = round(nrow(sample_marker)/8+2), height = 4)
 i}
 
